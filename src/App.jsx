328f9df2f469e58b9f135209e63679046174fb47
Najaf Ali Balti <najaf.balti000@gmail.com>
2024-10-16 17:18:35 +0500

    Again convert Routes from home to Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,13 @@
-import "./App.css";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-import {
-  About,
-  Homepage,
-  Cart,
-  Contact,
-  Checkout,
-  SingleProduct,
-  Shop,
-} from "./pages";
-import {
-  AdminDashboard,
-  ChefDashboard,
-  OwnerDashboard,
-  UserDashboard,
-} from "./Dashboard";
-import { Footer, Header } from "./components";
+import './App.css'
+import AppRoutes from './appRouting/AppRoutes'
 
 function App() {
+
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/product/:id" element={<SingleProduct />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/dashboard/admin" element={<AdminDashboard />} />
-        <Route path="/dashboard/user" element={<UserDashboard />} />
-        <Route path="/dashboard/owner" element={<OwnerDashboard />} />
-        <Route path="/dashboard/chef" element={<ChefDashboard />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  );
+    <>
+    <AppRoutes />
+    </>
+  )
 }
 
-export default App;
+export default App
