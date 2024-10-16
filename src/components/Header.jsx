9aefa990cd31ff9335323eb7504bd3392be0afa9
Najaf Ali Balti <najaf.balti000@gmail.com>
2024-10-16 16:52:37 +0500

    Header Secttion Completed!...

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,8 @@ import {
   ListItemText,
   Typography,
 } from "@mui/material";
-import { Search, ShoppingCart, Login } from "@mui/icons-material";
+import { Search, ShoppingCart, Login, Menu } from "@mui/icons-material";
+import MobileMenuPopup from "./MobileMenuPopup";
 
 // Demo Data
 const pages = [
@@ -36,137 +37,171 @@ const pages = [
     link: "/contact-us",
   },
 ];
-const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 const Header = () => {
+  const [mblMenuOpen, setMblMenuOpen] = React.useState(false);
   return (
-    <AppBar
-      position="static"
-      sx={{
-        backgroundColor: "transparent",
-        boxShadow: "0px 0px 5px #00000025",
-      }}
-    >
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          {/* Dekstop Menu */}
-          <Box
-            sx={{
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-              width: "100%",
-            }}
-          >
-            <Link to="/">
-              <Box
-                component="img"
-                sx={{
-                  width: { xs: "150px", md: "220px" },
-                  height: "auto",
-                }}
-                src="/src/assets/logo.png"
-                alt="Eat & Love"
-              />
-            </Link>
-
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "end",
-                alignItems: "center",
-                gap: "20px",
-              }}
-            >
-              {/* Navigation List */}
-              <List
-                sx={{
-                  display: { xs: "none", md: "flex" },
-                  gap: "16px",
-                  ml: "-8rem",
-                }}
-              >
-                {pages.map((item) => (
-                  <ListItem key={item.link} sx={{ width: "auto", padding: 0 }}>
-                    <Link
-                      to={item.link}
-                      style={{
-                        textDecoration: "none",
-                        color: "var(--primary-color)",
-                      }}
-                    >
-                      <ListItemText
-                        primary={item.name}
-                        primaryTypographyProps={{
-                          sx: {
-                            fontSize: "12px",
-                            textTransform: "uppercase",
-                            color: "var(--primary-color)",
-                          },
-                        }}
-                      />
-                    </Link>
-                  </ListItem>
-                ))}
-              </List>
+    <Box>
+      {mblMenuOpen ? (
+        <MobileMenuPopup pages={pages} setMblMenuOpen={setMblMenuOpen} />
+      ) : (
+        <AppBar
+          position="static"
+          sx={{
+            backgroundColor: "transparent",
+            boxShadow: "0px 0px 5px #00000025",
+          }}
+        >
+          <Container maxWidth="xl">
+            <Toolbar disableGutters>
+              {/* Dekstop Menu */}
               <Box
                 sx={{
                   display: "flex",
-                  gap: "8px",
+                  justifyContent: "space-between",
                   alignItems: "center",
+                  width: "100%",
                 }}
               >
-                {/* Search Icon */}
-                <Search
-                  sx={{
-                    color: "var(--font-color)",
-                    fontSize: { xs: "18px", sm: "25px" },
-                  }}
-                />
+                <Link to="/">
+                  <Box
+                    component="img"
+                    sx={{
+                      width: { xs: "150px", md: "220px" },
+                      height: "auto",
+                    }}
+                    src="/src/assets/logo.png"
+                    alt="Eat & Love"
+                  />
+                </Link>
 
-                {/* Cart Icon */}
                 <Box
                   sx={{
-                    position: "relative",
-                    mb: "-5px",
+                    display: "flex",
+                    justifyContent: "end",
+                    alignItems: "center",
+                    gap: "20px",
                   }}
                 >
-                  <ShoppingCart
+                  {/* Navigation List */}
+                  <List
                     sx={{
-                      color: "var(--font-color)",
-                      fontSize: { xs: "18px", sm: "25px" },
-                      zIndex: '10'
-                    }}
-                  />
-                  <Typography
-                    variant="p"
-                    sx={{
-                      fontSize: "8px",
-                      padding: "2px 6px",
-                      backgroundColor: "var(--secondary-color)",
-                      color: "var(--white-full)",
-                      borderRadius: "100px",
-                      position: "absolute",
-                      top: -10,
-                      right: -5,
+                      display: { xs: "none", md: "flex" },
+                      gap: "16px",
+                      ml: "-8rem",
                     }}
                   >
-                    2
-                  </Typography>
+                    {pages.map((item) => (
+                      <ListItem
+                        key={item.link}
+                        sx={{ width: "auto", padding: 0 }}
+                      >
+                        <Link
+                          to={item.link}
+                          style={{
+                            textDecoration: "none",
+                            color: "var(--primary-color)",
+                          }}
+                        >
+                          <ListItemText
+                            primary={item.name}
+                            primaryTypographyProps={{
+                              sx: {
+                                fontSize: "12px",
+                                textTransform: "uppercase",
+                                color: "var(--primary-color)",
+                              },
+                            }}
+                          />
+                        </Link>
+                      </ListItem>
+                    ))}
+                  </List>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      gap: "8px",
+                      alignItems: "center",
+                    }}
+                  >
+                    {/* Search Icon */}
+                    <Search
+                      sx={{
+                        color: "var(--font-color)",
+                        fontSize: { xs: "18px", sm: "25px" },
+                      }}
+                    />
+
+                    {/* Cart Icon */}
+                    <Box
+                      sx={{
+                        position: "relative",
+                        mb: "-5px",
+                      }}
+                    >
+                      <ShoppingCart
+                        sx={{
+                          color: "var(--font-color)",
+                          fontSize: { xs: "18px", sm: "25px" },
+                          zIndex: "2",
+                        }}
+                      />
+                      <Typography
+                        variant="p"
+                        sx={{
+                          fontSize: { xs: "6px", md: "8px" },
+                          padding: { xs: "2px 5px", md: "2px 6px" },
+                          backgroundColor: "var(--secondary-color)",
+                          color: "var(--white-full)",
+                          borderRadius: "100px",
+                          position: "absolute",
+                          top: -10,
+                          right: -5,
+                        }}
+                      >
+                        2
+                      </Typography>
+                    </Box>
+
+                    {/* Menu Bar For Phone */}
+                    <Button
+                      sx={{
+                        color: "var(--font-color)",
+                        padding: 0,
+                        margin: 0,
+                        minWidth: "20px",
+                        display: { xs: "flex", md: "none" },
+                      }}
+                      onClick={() => setMblMenuOpen(true)}
+                    >
+                      <Menu
+                        sx={{
+                          color: "var(--font-color)",
+                          fontSize: "18px",
+                          zIndex: "10",
+                        }}
+                      />
+                    </Button>
+                  </Box>
+                  <Button
+                    variant="contained"
+                    startIcon={<Login />}
+                    sx={{
+                      backgroundColor: "var(--secondary-color)",
+                      borderRadius: "20px",
+                      textTransform: "capitalize",
+                      display: { xs: "none", sm: "flex" },
+                    }}
+                  >
+                    Login
+                  </Button>
                 </Box>
               </Box>
-              <Button variant="contained" startIcon={<Login />} sx={{
-                backgroundColor: 'var(--secondary-color)',
-                borderRadius: '20px',
-                textTransform: 'capitalize'
-              }}>
-                Login
-              </Button>
-            </Box>
-          </Box>
-        </Toolbar>
-      </Container>
-    </AppBar>
+            </Toolbar>
+          </Container>
+        </AppBar>
+      )}
+    </Box>
   );
 };
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import Footer from "./Footer";
 import Header from "./Header";
+import MobileMenuPopup from "./MobileMenuPopup";
 import ProductCard from "./ProductCard";
 
 export {
     Header,
     Footer,
     ProductCard,
+    MobileMenuPopup,
 }
