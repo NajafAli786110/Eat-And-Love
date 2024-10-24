d4521eda074c5012967158f5a8b06cac6a77e9c4
Najaf Ali Balti <najaf.balti000@gmail.com>
2024-10-24 13:34:18 +0500

    CartItems issue Resolved

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -10,19 +10,49 @@ import {
 } from "@mui/material";
 import { IoBagCheckOutline } from "react-icons/io5";
 import { CartData } from "../appConstantData/ProductsData";
+import { useState } from "react";
 
-const totalPrice = cartItems.reduce(
+const totalPrice = CartData.reduce(
   (acc, item) => acc + item.price * item.quantity,
   0
 );
 
 const Checkout = () => {
+  // checkoutData
+  const [checkoutData, setCheckoutData] = useState({
+    fullName: "",
+    email: "",
+    address: "",
+    city: "",
+    postCode: "",
+    country: "",
+  });
+
+  // onChange Handler
+  const onChangeHandler = (e) => {
+    setCheckoutData({
+      ...checkoutData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const onSubmitHandler = () => {
+    console.log(checkoutData);
+    setCheckoutData({
+      fullName: "",
+      email: "",
+      address: "",
+      city: "",
+      postCode: "",
+      country: "",
+    });
+  };
   return (
     <Box
       sx={{
         display: "flex",
         flexDirection: { xs: "column", md: "row" },
-        gap: {xs: "14px", md: "30px"},
+        gap: { xs: "14px", md: "30px" },
         padding: { xs: "20px 10px 100px 10px", sm: "60px 20px 100px 20px" },
         color: "black",
       }}
@@ -52,6 +82,9 @@ const Checkout = () => {
                 borderColor: "var(--white-full)",
               },
             }}
+            name="fullName"
+            value={checkoutData.fullName}
+            onChange={onChangeHandler}
           />
           <TextField
             label="Email Address"
@@ -62,6 +95,9 @@ const Checkout = () => {
                 borderColor: "var(--white-full)",
               },
             }}
+            name="email"
+            value={checkoutData.email}
+            onChange={onChangeHandler}
           />
           <TextField
             label="Address"
@@ -72,6 +108,9 @@ const Checkout = () => {
                 borderColor: "var(--white-full)",
               },
             }}
+            name="address"
+            value={checkoutData.address}
+            onChange={onChangeHandler}
           />
           <TextField
             label="City"
@@ -82,6 +121,9 @@ const Checkout = () => {
                 borderColor: "var(--white-full)",
               },
             }}
+            name="city"
+            value={checkoutData.city}
+            onChange={onChangeHandler}
           />
           <TextField
             label="Postal Code"
@@ -92,6 +134,9 @@ const Checkout = () => {
                 borderColor: "var(--white-full)",
               },
             }}
+            name="postCode"
+            value={checkoutData.postCode}
+            onChange={onChangeHandler}
           />
           <TextField
             label="Country"
@@ -102,6 +147,9 @@ const Checkout = () => {
                 borderColor: "var(--white-full)",
               },
             }}
+            name="country"
+            value={checkoutData.country}
+            onChange={onChangeHandler}
           />
         </Box>
       </Box>
@@ -121,20 +169,29 @@ const Checkout = () => {
         <Divider sx={{ my: 2, backgroundColor: "var(--white-full)" }} />
 
         {/* Order Summary Items */}
-        <List sx={{
-          padding: '0px'
-        }}>
+        <List
+          sx={{
+            padding: "0px",
+          }}
+        >
           {CartData.map((item) => (
-            <ListItem key={item.id} sx={{
-              padding: '10px 0px'
-            }}>
-              <img src={item.imageUrl} alt={item.name} style={{
-                height: '40px',
-                width: '40px',
-                objectFit: 'cover',
-                borderRadius: '100px',
-                marginRight: '10px'
-              }} />
+            <ListItem
+              key={item.id}
+              sx={{
+                padding: "10px 0px",
+              }}
+            >
+              <img
+                src={item.imageUrl}
+                alt={item.name}
+                style={{
+                  height: "40px",
+                  width: "40px",
+                  objectFit: "cover",
+                  borderRadius: "100px",
+                  marginRight: "10px",
+                }}
+              />
               <ListItemText
                 primary={`${item.name} x ${item.quantity}`}
                 secondary={`$${item.price.toFixed(2)} each`}
@@ -170,6 +227,7 @@ const Checkout = () => {
             padding: "12px",
             marginTop: "20px",
           }}
+          onClick={onSubmitHandler}
         >
           Complete Purchase
         </Button>
